68be17c38eeeb70c1faf6afd825afa4c36ead6df
Rich Alberth <41269+ralberth@users.noreply.github.com>
2025-06-17 08:57:05 -0400

    cleaner main screen

--- app/src/main/java/org/ralberth/areyouok/ui/mainscreen/MainScreen.kt
+++ app/src/main/java/org/ralberth/areyouok/ui/mainscreen/MainScreen.kt
@@ -1,26 +1,26 @@
 package org.ralberth.areyouok.ui.mainscreen
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material.icons.outlined.Edit
-import androidx.compose.material3.BottomAppBarDefaults.ContentPadding
+import androidx.compose.material.icons.automirrored.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.Face
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Refresh
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.modifier.modifierLocalConsumer
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
@@ -56,38 +56,47 @@ fun MainScreen(
         }
 
         SummaryRow(
+            leftIcon = Icons.Filled.Refresh,
             label = "Duration",
+            value = "${uiState.countdownLength} minutes",
             onEdit = { navController.navigate("durationselect") }
-        ) {
-            Text("${uiState.countdownLength} mins")
-        }
+        )
 
+        HorizontalDivider()
+
+        val phoneValue = when {
+            uiState.phoneName.isEmpty() && uiState.phoneNumber.isEmpty() -> "Pick a contact"
+            uiState.phoneName.isEmpty() -> uiState.phoneNumber   // uiState.phoneNumber.length guaranteed > 0 at this point
+            else -> "${uiState.phoneName}   ${uiState.phoneNumber}"
+        }
         SummaryRow(
+            leftIcon = Icons.Filled.Face,
             label = "Contact",
+            value = phoneValue,
             onEdit = { askForContactPhoneNumber() }
-        ) {
-            when {
-                uiState.phoneName.length == 0 && uiState.phoneNumber.length == 0 -> Text("Pick a contact")
-                uiState.phoneName.length == 0 -> Text(uiState.phoneNumber)   // uiState.phoneNumber.length guaranteed > 0 at this point
-                else -> Text(uiState.phoneName)
-            }
-        }
+        )
+
+        HorizontalDivider()
 
         SummaryRow(
+            leftIcon = Icons.Filled.Home,
             label = "Location",
+            value = if (uiState.location == "") "Pick a location" else uiState.location,
             onEdit = { navController.navigate("locationselect") }
-        ) {
-            Text(if (uiState.location == "") "Pick a location" else uiState.location)
-        }
+        )
 
-        SummaryRow("Enable") {
+        HorizontalDivider()
+
+        Row(
+            modifier = Modifier.padding(start = 18.dp, end = 18.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Text("Enable")
+            Spacer(Modifier.weight(1f))
             Switch(
-                enabled = appIsUsable && uiState.phoneNumber.length > 0,
+                enabled = appIsUsable && uiState.phoneNumber.isNotEmpty(),
                 checked = uiState.countdownStart != null,
-                onCheckedChange = {
-                    viewModel.updateEnabled(it)
-//                    navController.navigate("countdown")
-                }
+                onCheckedChange = { viewModel.updateEnabled(it) }
             )
         }
     }
@@ -111,52 +120,27 @@ fun NeedPermissionBanner(type: String) {
 
 
 @Composable
-fun EditButton(
-    enabled: Boolean,
-    onClick: () -> Unit
-) {
-    TextButton(
-        enabled = enabled,
-        onClick = onClick
-    ) {
-        Icon(
-            Icons.Filled.Edit,
-            "Change")
-    }
-}
-
-
-@Composable
 fun SummaryRow(
+    leftIcon: ImageVector,
     label: String,
-    onEdit: (() -> Unit)? = null,
-    content: @Composable () -> Unit
+    value: String,
+    onEdit: () -> Unit
 ) {
     Row(
-        modifier = Modifier.padding(start = 18.dp, end = 18.dp),
+        modifier = Modifier
+            .padding(start = 18.dp, end = 18.dp)
+            .clickable(true, onClickLabel="click", onClick = onEdit),
         verticalAlignment = Alignment.CenterVertically
     ) {
-        Text(label, fontWeight = FontWeight.Bold)
+        Icon(
+            leftIcon,
+            "left",
+            modifier = Modifier.padding(end = 18.dp))
+        Column {
+            Text(label, fontWeight = FontWeight.Bold)
+            Text(value)
+        }
         Spacer(Modifier.weight(1f))
-        content()
-        if (onEdit != null)
-            EditButton(enabled = true/* FIXME */, onClick = onEdit)
+        Icon(Icons.AutoMirrored.Filled.KeyboardArrowRight, "Change")
     }
 }
-
-
-//@Composable
-//fun EnableDisableToggle(appIsUsable: Boolean, isEnabled: Boolean, onChange: (Boolean) -> Unit) {
-//    Row(
-//        modifier = Modifier.padding(18.dp),
-//        verticalAlignment = Alignment.CenterVertically
-//    ) {
-//        Text("Enable", fontWeight = FontWeight.Bold)
-//        Spacer(Modifier.weight(1f))
-//        Switch(
-//            enabled = appIsUsable,
-//            checked=isEnabled,
-//            onCheckedChange=onChange
-//        )
-//    }
-//}
